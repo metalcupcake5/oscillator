ed41eb490066dc0a2e2fa4c3cf38c5ec316d50a7
metalcupcake5 <43897385+metalcupcake5@users.noreply.github.com>
2025-10-17 15:22:23 -0400

    correct calculations

--- data.m
+++ data.m
@@ -7,25 +7,28 @@ recorded_time = recorded_data.time(time_start:time_end);
 recorded_acceleration = recorded_data.linear_acceleration_y(time_start:time_end);
 
 mass = .3255;
-k = 0.01279291 % spring constant, N/m
-sigma = -0.0079 % exponential decay rate, 1/s
+k = 6.0283 % spring constant, N/m
+sigma = 0.0079 % exponential decay rate, 1/s
 omega_n_1 = sqrt(k / mass) % natural freq from measured mass/stiffness, rad/s
-omega_n_2 = 1; % natural freq from accel, rad/s
+omega_d = 1 / 1.36 % damped freq, rad/s, 1 / period of oscillation
+
 zeta = sigma / omega_n_1 % damping ratio, dimensionless
-c = zeta * 2 * sqrt(k * mass) % damping coeff, (N * s)/m
-omega_d = omega_n_1 * sqrt(1 - zeta ^ 2) % damped freq, rad/s
+
+c = zeta * 2 * omega_n_1 * mass % damping coeff, (N * s)/m
+omega_n_2 = sqrt(1 - zeta^2)/omega_d % natural freq from accel, rad/s
 
 % measured data (spring)
-force = [0 3.2 12.3 21.6 30.6 38.9 48.4].*0.001; %convert g to kg
+force = [0 3.2 12.3 21.6 30.6 38.9 48.4].*0.001*9.8; %convert g to kg
 displacement = [0 50 100 150 200 250 300].*0.01; %convert cm to m
 
 %curve fitting (with Curve Fitting Toolbox).
-[xData, yData] = prepareCurveData( displacement, force );
-ft = fittype( 'poly2' );
-[fitresult, gof] = fit( xData, yData, ft );
+p = polyfit(force, displacement, 1)
 
 figure();
-plot( fitresult, xData, yData );
+plot(linspace(-0.5, 3), p(1) * linspace(-0.5, 3) + p(2));
+hold on;
+scatter(force, displacement);
+xlim([-0.1, 0.7])
 title("Experimental Data (Spring Characterization)")
 xlabel("Displacement (m)");
 ylabel("Force (N)");
